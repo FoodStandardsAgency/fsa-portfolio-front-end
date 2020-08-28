9e0b0e89f8718d20b9f9eb9d6db7df834e19a6bf
Sam Richards <sam@ipassenger.io>
2020-08-28 13:15:24 +0100

    Added missing flash code.

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const path 		= require('path');
 const express 	= require('express');
 const session = require('express-session');
 //const session	= require('client-sessions');
+const flash = require('connect-flash');
 const nunjucks  = require('nunjucks');
 const queries 	= require('./app/queries');
 const moment	= require('moment');
@@ -17,6 +18,25 @@ const morgan = require('morgan');
 const favicon = require('serve-favicon');
 
 const app = express();
+
+// Flash middleware
+app.use(flash());
+
+// Set up local vars for template layout
+app.use(function (req, res, next) {
+	// Read any flashed errors and save
+	// in the response locals
+	res.locals.error = req.flash('error_msg');
+
+	// Check for simple error string and
+	// convert to layout's expected format
+	var errs = req.flash('error');
+	for (var i in errs) {
+		res.locals.error.push({ message: 'An error occurred', debug: errs[i] });
+	}
+
+	next();
+});
 
 const port = process.env.PORT || 3100;
 const dev = process.env.NODE_ENV !== 'production';
