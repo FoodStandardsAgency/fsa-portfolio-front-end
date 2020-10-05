c2762614282d76da6109addb1277c7b1ef675eda
Sam Richards <sam@ipassenger.io>
2020-10-05 22:03:03 +0100

    Update label configuration

--- app/queries.js
+++ app/queries.js
@@ -9,25 +9,31 @@ const pool = new Pool()
 const q = {
 	max_id:				'SELECT max(project_id) from latest_projects',
 	odd_people: 'select * from odd_people order by g6team, surname',
+
+	portfolio_projects_url: (portfolio, filter) => `Projects?portfolio=${portfolio}&filter=${filter}`,
+	portfolio_config_url: (portfolio) => `PortfolioConfiguration/${portfolio}`,
+	project_url: (projectId) => `Projects/${projectId}`
 	
 }
 
 // Export promises
 module.exports = {
-	current_projects: (portfolio) => backend.api(`Projects?portfolio=${portfolio}&filter=current`),
-	completed_projects: (portfolio) => backend.api(`Projects?portfolio=${portfolio}&filter=complete`),
-	latest_projects: (portfolio) => backend.api(`Projects?portfolio=${portfolio}&filter=latest`),
+	// Actions
+	current_projects: (portfolio) => backend.api(q.portfolio_projects_url(portfolio, 'current')),
+	completed_projects: (portfolio) => backend.api(q.portfolio_projects_url(portfolio, 'complete')),
+	latest_projects: (portfolio) => backend.api(q.portfolio_projects_url(portfolio, 'latest')),
+	new_projects: (portfolio) => backend.api(q.portfolio_projects_url(portfolio, 'new')),
+	load_project: (projectId) => backend.api(q.project_url(projectId)),
+	load_updates: (projectId) => backend.api(`${q.project_url(projectId)}/updates`),
+	load_related: (projectId) => backend.api(`${q.project_url(projectId)}/related`),
+	load_dependant: (projectId) => backend.api(`${q.project_url(projectId)}/dependant`),
 	max_id: (portfolio) => backend.api(`PortfolioConfiguration/MaxId?portfolio=${portfolio}`),
 	oddleads: (text, params) => backend.api('Projects/Legacy/ODDLeads'),
 	generic_query: 		(text, params) => pool.query(text, params),
 	unmatched_leads: (text, params) => backend.api('Projects/Legacy/UnmatchedODDLeads'),
 	odd_people:			(text, params) => pool.query(q.odd_people),
-	new_projects: (portfolio) => backend.api(`Projects?portfolio=${portfolio}&filter=new`),
-	load_project: (projectId) => backend.api(`Projects/${projectId}`),
-	load_updates: (projectId) => backend.api(`Projects/${projectId}/updates`),
-	load_related: (projectId) => backend.api(`Projects/${projectId}/related`),
-	load_dependant: (projectId) => backend.api(`Projects/${projectId}/dependant`),
 	update_label: (portfolio, field, label, included) => backend.api.post('PortfolioConfiguration/Label', { json: { portfolio: portfolio, field: field, label: label, included: included } }),
-	portfolio_config: (portfolio) => backend.api(`PortfolioConfiguration?portfolio=${portfolio}`)
+	portfolio_config: (portfolio) => backend.api(q.portfolio_config_url(portfolio)),
+	portfolio_config_update: (portfolio, data) => backend.api.patch(q.portfolio_config_url(portfolio), { json: data })
 
 }
