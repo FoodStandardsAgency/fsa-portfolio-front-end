ed36732db3cd426e89fb541687cd33372534ffdc
Sam Richards <sam@ipassenger.io>
2020-10-23 17:15:59 +0100

    Dates view and update improvements

--- app/project_view.js
+++ app/project_view.js
@@ -46,58 +46,11 @@ async function project_view(req, res) {
 			if (project.spent != null && project.spent != '') { var spent = project.spent.split(","); }
 			else { var spent = 0; }
 
-			/* Project dates */
-			if (project.start_date != null && project.start_date != '00/00/0000') {
-				var isd = true;
-				var isd_month = months[parseInt(project.start_date.slice(3, 5)) - 1];
-				var isd_year = project.start_date.slice(6, 10);
-			}
-			else { var isd_month = ''; var isd_year = ''; }
-
-			if (project.actstart != null && project.actstart != '00/00/0000') {
-				var asd = true;
-				var asd_day = project.actstart.slice(0, 2);
-				var asd_month = months[parseInt(project.actstart.slice(3, 5)) - 1];
-				var asd_year = project.actstart.slice(6, 10);
-			} else { var asd_month = ''; var asd_year = ''; }
-
-			if (project.expendp != null && project.expendp != '00/00/0000') {
-				var eedp = true;
-				var eedp_day = project.expendp.slice(0, 2);
-				var eedp_month = months[parseInt(project.expendp.slice(3, 5)) - 1];
-				var eedp_year = project.expendp.slice(6, 10);
-			}
-			else { var eed_month = ''; var eed_year = ''; }
-
-			if (project.expend != null && project.expend != '00/00/0000') {
-				var eed = true;
-				var eed_month = months[parseInt(project.expend.slice(3, 5)) - 1];
-				var eed_year = project.expend.slice(6, 10);
-			}
-			else { var eed_month = ''; var eed_year = ''; }
-
-			if (project.hardend != null && project.hardend != '00/00/0000') {
-				var aed = true;
-				var aed_day = project.hardend.slice(0, 2);
-				var aed_month = months[parseInt(project.hardend.slice(3, 5)) - 1];
-				var aed_year = project.hardend.slice(6, 10);
-			}
-			else { var aed_month = ''; var aed_year = ''; }
-
-			var dates = [isd_month, isd_year, asd_day, asd_month, asd_year, eed_month, eed_year, aed_day, aed_month, aed_year, isd, asd, eed, aed, eedp, eedp_day, eedp_month, eedp_year];
-
 			/*Budget type*/
 			if (project.budgettype == 'none' || project.budgettype == undefined) { var budgettype = 'Not set' }
 			else if (project.budgettype == 'admin') { var budgettype = 'Admin' }
 			else if (project.budgettype == 'progr') { var budgettype = 'Programme' }
 			else { var budgettype = 'Capital' }
-
-			var comp_date = project.first_completed;
-			if (!comp_date) { comp_date = ''; }
-			else {
-				comp_date = String(comp_date);
-				comp_date = comp_date.split(' ');
-			}
 
 			var rels = await queries.load_related(project_id);
 			var deps = await queries.load_dependant(project_id);
@@ -125,8 +78,6 @@ async function project_view(req, res) {
 				"budgettype": budgettype,
 				"budget": currencyFormat(project.budget),
 				"spent": currencyFormat(project.spent),
-				"dates": dates,
-				"comp": comp_date,
 				"link": links,
 				"sess": sess
 			});
