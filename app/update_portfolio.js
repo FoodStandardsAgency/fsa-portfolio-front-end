340b079a619a7f18460f0a35b3023801aa8310c5
Sam Richards <sam@ipassenger.io>
2020-10-26 18:27:10 +0000

    Some fields can always be viewed by editor

--- app/update_portfolio.js
+++ app/update_portfolio.js
@@ -18,41 +18,36 @@ function handleError(error) {
 
 async function edit(req, res) {
 	try {
-		if (req.session.user == 'portfolio') {
+		var isAdmin = (req.session.user === 'portfolio');
+		var portfolio = req.params.portfolio;
+		var project_id = req.params.project_id;
+		var result = await queries.load_project(project_id, { includeConfig: true, includeOptions: true, includeLastUpdate: true });
+		var project = result.body.project;
+		var config = result.body.config;
+		var options = result.body.options;
 
-			var isAdmin = (req.session.user === 'portfolio');
-			var portfolio = req.params.portfolio;
-			var project_id = req.params.project_id;
-			var result = await queries.load_project(project_id, { includeConfig: true, includeOptions: true, includeLastUpdate: true });
-			var project = result.body.project;
-			var config = result.body.config;
-			var options = result.body.options;
+		var fieldGroups = _.chain(config.labels)
+			.orderBy("grouporder", "fieldorder")
+			.groupBy("fieldgroup")
+			.map((value, key) => ({
+				"fieldgroup": key,
+				labels: value,
+				display: (_.findIndex(value, { included: true }) >= 0)
+			}))
+			.value();
 
-			var fieldGroups = _.chain(config.labels)
-				.orderBy("grouporder", "fieldorder")
-				.groupBy("fieldgroup")
-				.map((value, key) => ({
-					"fieldgroup": key,
-					labels: value,
-					display: (_.findIndex(value, { included: true }) >= 0)
-				}))
-				.value();
+		//console.log(fieldGroups);
 
-			//console.log(fieldGroups);
-
-			res.render('add-edit-project', {
-				"title": "Edit project",
-				"user": req.session.user, // need access-level to determine whether user can add projects
-				"isAdmin": isAdmin,
-				"project": project,
-				"options": options,
-				"sess": req.session,
-				"portfolio": portfolio,
-				"fieldgroups": fieldGroups
-			});
-
-		}
-		else { res.render('error_page', { message: 'You are not authorised to view this page' }); }
+		res.render('add-edit-project', {
+			"title": "Edit project",
+			"user": req.session.user, // need access-level to determine whether user can add projects
+			"isAdmin": isAdmin,
+			"project": project,
+			"options": options,
+			"sess": req.session,
+			"portfolio": portfolio,
+			"fieldgroups": fieldGroups
+		});
 	}
 	catch (error) {
 		handleError(error);
