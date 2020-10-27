f28302cf029e252f2ed96864e0cc2f3503c01889
Sam Richards <sam@ipassenger.io>
2020-10-27 20:20:08 +0000

    Collection input names were incorrect

--- app/filter_view.js
+++ app/filter_view.js
@@ -33,13 +33,14 @@ async function view(req, res) {
 async function getResults(req, res) {
 	try {
 		var portfolio = req.params.portfolio;
+		console.log(req.body);
+
 		var queryResult = await queries.portfolio_filtered_projects(portfolio, req.body);
 		var optionsResult = await queries.portfolio_filter_options(portfolio);
 		var portfolioconfig = optionsResult.body.config;
 		var options = optionsResult.body.options;
 		var fieldGroups = config.getFieldGroups(portfolioconfig);
 
-		//console.log(queryResult.body);
 		var label_priority_main = portfolioconfig.labels.find(l => l.field == 'priority_main').label;
 
 		res.render('filter_view', {
