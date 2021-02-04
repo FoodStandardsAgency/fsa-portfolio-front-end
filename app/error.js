a6a9706278f49a8ef48010600fbc27eaf6ad111f
Sam Richards <sam.richards@food.gov.uk>
2021-02-04 16:42:43 +0000

    Handle 400 errors - avoid whitescreen by default.

--- app/error.js
+++ app/error.js
@@ -1,22 +1,37 @@
 function handleError(error, res) {
-	if (res && error.response && error.response.statusCode == 403) {
+	if (res && error.response) {
 		if (error.message) console.log(error.message);
-		handleUnauthorised(res);
-		return true;
+		if (error.response.statusCode == 403) {
+			handleUnauthorised(res);
+			return true;
+		}
+		else if (error.response.statusCode == 400) {
+			handleBadRequest(res, error.message);
+			return true;
+		}
+
 	}
 	else {
 
 		console.log('***************************');
-		if (error.message) console.log(error.message);
-		if (error.stack) console.log(error.stack);
+		var message = "Unknown error."
+		var statusCode = "";
+		if (error.message) {
+			message = error.message;
+			console.log(error.message);
+		}
+		if (error.stack) {
+			console.log(error.stack);
+		}
 		if (error.response) {
+			statusCode = ` (error code ${error.response.statusCode})`;
 			console.log('Response from server:');
 			console.log(error.response.url);
 			console.log(error.response.body.ExceptionMessage);
 			console.log(error.response.body);
 		}
 		console.log('***************************');
-		//res.render('error_page', { message: `System error. Please contact support (error code ${error.response.statusCode}).` });
+		res.render('error_page', { message: `System error. Please contact support${statusCode}.` });
 		return false;
 	}
 }
@@ -30,6 +45,10 @@ function handleUnauthorised(res, action) {
 		console.log('User not authorised.')
 }
 
+function handleBadRequest(res, message) {
+	message = message.replace("Response code 400", "");
+	res.render('error_page', { message: `There was a problem in the data ${message}` });
+}
 
 
-module.exports = { handleError, handleUnauthorised };
+module.exports = { handleError, handleUnauthorised, handleBadRequest };
